95858e76ee30bc62d9c9fa4405d97ca4ada1e8d0
gulernurdonmez <gule0153@stud.kea.dk>
2023-05-10 13:28:19 +0200

    minor change that makes this shit work

--- Program.cs
+++ Program.cs
@@ -8,19 +8,19 @@ namespace Linq
     {
         public static void Main(String[] args)
         {
-            List<Employee> employees = new List<Employee>(); 
+            List<Employee> employees = new List<Employee>() 
             {
-                new Employee { Name = "Güler", Department = "Sales", Salary = 50000.00m }; 
-                new Employee { Name = "Chris", Department = "Marketing", Salary = 45000.00m };
-                new Employee { Name = "Lasse", Department = "Sales", Salary = 60000.00m };
-                new Employee { Name = "David", Department = "Sales", Salary = 55000.00m };
-                new Employee { Name = "Troels", Department = "Marketing", Salary = 40000.00m };
-                new Employee { Name = "Kenneth", Department = "Sales", Salary = 70000.00m };
-                new Employee { Name = "Maja", Department = "Sales", Salary = 65000.00m };
-                new Employee { Name = "Victor", Department = "Marketing", Salary = 42000.00m };
-                new Employee { Name = "Niels", Department = "Sales", Salary = 55000.00m };
-                new Employee { Name = "Markus", Department = "Sales", Salary = 48000.00m };
-            }
+                new Employee { Name = "Güler", Department = "Sales", Salary = 50000.00m },
+                new Employee { Name = "Chris", Department = "Marketing", Salary = 45000.00m },
+                new Employee { Name = "Lasse", Department = "Sales", Salary = 60000.00m },
+                new Employee { Name = "David", Department = "Sales", Salary = 55000.00m },
+                new Employee { Name = "Troels", Department = "Marketing", Salary = 40000.00m },
+                new Employee { Name = "Kenneth", Department = "Sales", Salary = 70000.00m },
+                new Employee { Name = "Maja", Department = "Sales", Salary = 65000.00m },
+                new Employee { Name = "Victor", Department = "Marketing", Salary = 42000.00m },
+                new Employee { Name = "Niels", Department = "Sales", Salary = 55000.00m },
+                new Employee { Name = "Markus", Department = "Sales", Salary = 48000.00m },
+            };
 
             //Where method. 
             var salesEmployees = employees.Where(e => e.Department == "Sales");
