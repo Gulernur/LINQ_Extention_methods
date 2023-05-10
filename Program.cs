65f5ec39f3fc48edb034228bab4a137998140b57
gulernurdonmez <gule0153@stud.kea.dk>
2023-05-10 13:17:40 +0200

    added orderBt, ThenBy, First & Last methods

--- Program.cs
+++ Program.cs
@@ -10,20 +10,31 @@ namespace Linq
         {
             List<Employee> employees = new List<Employee>(); 
             {
-                new Employee { Name = "Alice", Department = "Sales", Salary = 50000.00m }; 
-                new Employee { Name = "Bob", Department = "Marketing", Salary = 45000.00m };
-                new Employee { Name = "Charlie", Department = "Sales", Salary = 60000.00m };
-                new Employee { Name = "Dave", Department = "Sales", Salary = 55000.00m };
-                new Employee { Name = "Eve", Department = "Marketing", Salary = 40000.00m };
-                new Employee { Name = "Frank", Department = "Sales", Salary = 70000.00m };
-                new Employee { Name = "Grace", Department = "Sales", Salary = 65000.00m };
-                new Employee { Name = "Henry", Department = "Marketing", Salary = 42000.00m };
-                new Employee { Name = "Isabelle", Department = "Sales", Salary = 55000.00m };
-                new Employee { Name = "Jack", Department = "Sales", Salary = 48000.00m };
+                new Employee { Name = "Güler", Department = "Sales", Salary = 50000.00m }; 
+                new Employee { Name = "Chris", Department = "Marketing", Salary = 45000.00m };
+                new Employee { Name = "Lasse", Department = "Sales", Salary = 60000.00m };
+                new Employee { Name = "David", Department = "Sales", Salary = 55000.00m };
+                new Employee { Name = "Troels", Department = "Marketing", Salary = 40000.00m };
+                new Employee { Name = "Kenneth", Department = "Sales", Salary = 70000.00m };
+                new Employee { Name = "Maja", Department = "Sales", Salary = 65000.00m };
+                new Employee { Name = "Victor", Department = "Marketing", Salary = 42000.00m };
+                new Employee { Name = "Niels", Department = "Sales", Salary = 55000.00m };
+                new Employee { Name = "Markus", Department = "Sales", Salary = 48000.00m };
             }
 
             //Where method. 
             var salesEmployees = employees.Where(e => e.Department == "Sales");
+
+            //OrderBy method & ThenBy method. 
+            var sortedEmployees = salesEmployees.OrderBy(e => e.Salary).ThenBy(e => e.Name);
+
+            //First employee in the list. 
+            var firstEmployee = sortedEmployees.First();
+            Console.WriteLine("First employee: {0} ({1}, {2:C})", firstEmployee.Name, firstEmployee.Department, firstEmployee.Salary);
+
+            //Last employee in the list. 
+            var lastEmployee = sortedEmployees.Last(); 
+            Console.WriteLine("Last employee: {0} ({1}, {2:C})", lastEmployee.Name, lastEmployee.Department, lastEmployee.Salary);
 
 
             Console.ReadLine(); 
